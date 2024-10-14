ff6957c67a62523f054c3a1818cc801bb07f8ceb
Sergeeva Anna <sirgeeva1@gmail.com>
2024-10-14 10:28:51 +0500

    fix: MealTo  changed

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -17,7 +17,23 @@ public class MealTo {
         this.calories = calories;
         this.excess = excess;
     }
-    
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
     @Override
     public String toString() {
         return "MealTo{" +
