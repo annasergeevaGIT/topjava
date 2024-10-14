ca0caaf926a760d0cc2136a526cc3a0bbfc0ed4f
Sergeeva Anna <sirgeeva1@gmail.com>
2024-10-14 10:08:57 +0500

    fix: MealServlet  changed

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.model.UserMealWithExceed;
+import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
@@ -9,24 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalTime;
-import java.util.List;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
-    private static final Logger log = getLogger(MealServlet.class);
-    private static final int CALORIES_PER_DAY = 2000; // calories norm
+    private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("display meals");
-
-        // Convert to List<MealTo> with the excess flag calculated
-        List<UserMealWithExceed> mealsTo = MealsUtil.filteredByStreams(MealsUtil.MEAL_LIST, LocalTime.of(7, 0), LocalTime.of(12, 0), CALORIES_PER_DAY);
-        request.setAttribute("meals", mealsTo);
-
-        // Forward to JSP
+        log.info("forward to meals");
+        request.setAttribute("meals", MealsUtil.getTos(MealsUtil.meals, MealsUtil.DEFAULT_CALORIES_PER_DAY));
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
